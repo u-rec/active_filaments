5fc279b9bd0587a17dba490c6e93b150333f19c7
jurek <jerzy.denisiewicz@vavatech.pl>
2023-01-26 16:24:21 +0100

    all forces

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
 
 
 class G:
-    N = 5
+    N = 30
     R = 5E-1
     ks = 1E2
     k = 1E2
@@ -12,6 +13,8 @@ class G:
     dt = 5E-3
     L_pol = (N-1) * l0
     ksi_p = 0.1
+    Pe = 1E-3
+    fp = Pe * a / (L_pol**2)
     kappa = ksi_p * a * L_pol
 
 
@@ -57,15 +60,15 @@ class Filament:
     def norms(self):
         x = self.x()
         y = self.y()
-        return np.sqrt(np.multiply(x, x) + np.multiply(y,y))
+        return np.sqrt(np.multiply(x, x) + np.multiply(y, y))
 
     @staticmethod
     def normalize(a):
         x = np.asarray(a).T[0]
         y = np.asarray(a).T[1]
-        len = np.sqrt(np.multiply(x, x) + np.multiply(y,y)).T
-        len[len == 0] = 1
-        return np.asmatrix([x/len, y/len]).T
+        length = np.sqrt(np.multiply(x, x) + np.multiply(y, y)).T
+        length[length == 0] = 1
+        return np.asarray([x/length, y/length]).T
 
 
     def rotated(self):
@@ -101,10 +104,10 @@ class Filament:
         return rot
 
     def angle_force(self):
-        force_third_x = - G.ks * self.shift(self.angles()) * np.asarray(self.direction_third_particle_angle_force()).T[0]
-        force_third_y = - G.ks * self.shift(self.angles()) * np.asarray(self.direction_third_particle_angle_force()).T[1]
-        force_first_x = - G.ks * self.deshift(self.angles()) * np.asarray(self.direction_first_particle_angle_force()).T[0]
-        force_first_y = - G.ks * self.deshift(self.angles()) * np.asarray(self.direction_first_particle_angle_force()).T[1]
+        force_third_x = - G.ks * self.shift(self.angles()) * self.direction_third_particle_angle_force().T[0]
+        force_third_y = - G.ks * self.shift(self.angles()) * self.direction_third_particle_angle_force().T[1]
+        force_first_x = - G.ks * self.deshift(self.angles()) * self.direction_first_particle_angle_force().T[0]
+        force_first_y = - G.ks * self.deshift(self.angles()) * self.direction_first_particle_angle_force().T[1]
         force_second_x = - (self.shift(force_first_x) + self.deshift(force_third_x))
         force_second_x[0] = 0
         force_second_x[-1] = 0
@@ -113,28 +116,45 @@ class Filament:
         force_second_y[-1] = 0
         return np.asarray([force_first_x + force_second_x + force_third_x, force_first_y + force_second_y + force_third_y]).T
 
+    def r_each_to_each(self):
+        diff_x = np.ones((G.N, G.N)) * np.asarray(self.x()) - np.ones((G.N, G.N)) * np.asarray(self.x()).T
+        diff_y = np.ones((G.N, G.N)) * np.asarray(self.y()) - np.ones((G.N, G.N)) * np.asarray(self.y()).T
+        norm = np.sqrt(diff_x ** 2 + diff_y ** 2)
+        np.putmask(norm, norm == 0, 1)
+        return 2 * G.R * np.asarray([diff_x/norm, diff_y/norm])
+
     def ev_force(self):
         diff_x = np.ones((G.N, G.N)) * np.asarray(self.x()) - np.ones((G.N, G.N)) * np.asarray(self.x()).T
         diff_y = np.ones((G.N, G.N)) * np.asarray(self.y()) - np.ones((G.N, G.N)) * np.asarray(self.y()).T
         near = np.logical_and(diff_x ** 2 + diff_y ** 2 < 4 * (G.R**2), np.identity(G.N) == 0)
+        directed_r = self.r_each_to_each()
 
         force_x = np.zeros((G.N, G.N))
         force_y = np.zeros((G.N, G.N))
-        np.putmask(force_x, near, - G.k * (diff_x)
-        force_y[near] = -G.k * diff_y
-        return
+        np.putmask(force_x, near, - G.k * (diff_x - directed_r[0]))
+        np.putmask(force_y, near, - G.k * (diff_y - directed_r[1]))
+        return np.asarray([force_x.sum(1), force_y.sum(1)]).T
+
+    def active_force(self):
+        return G.fp * self.deshift(np.asarray(self.vectors()))
+
+    def make_step(self):
+        all_force = self.bond_force() + self.angle_force() + self.ev_force() + self.active_force()
+        all_force *= G.dt
+        all_force += np.sqrt(G.a * G.dt) * np.random.normal(size=(G.N, 2))
+        self.particles += all_force
+        return self.particles
+
 
 
 
 f = Filament()
 f.particles[1,1] = 1
-print(f.particles)
-print(f.bond_force())
-print(f.angles())
-print(f.angle_force())
-print(f.ev_force())
 
-def main():
-    return
+for i in range(1000):
+    table = np.asarray(f.make_step()).T
+    plt.plot(table[0], table[1])
+    plt.savefig(f"figures/{i:05d}.png")
+    plt.close()
 
 
